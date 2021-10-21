919bbd323cd6659f6034bfde5e288904b507bb17
Kevin Tipantiza <kevinftipantiza@gmail.com>
2021-10-20 20:39:40 -0500

    guardar en localstorage y estilos

--- src/Components/Contactos.jsx
+++ src/Components/Contactos.jsx
@@ -1,37 +1,40 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { ContactosReducer } from "../Reducers/ContactosReducer";
 import FormularioAdd from "./FormularioAdd";
 import TablaContactos from "./TablaContactos";
 
-const contactos = [
-  {
-    id: "asc123",
-    nombre: "Kevin",
-    numero: "09122348",
-  },
-  {
-    id: "asc1pp76",
-    nombre: "Kevin",
-    numero: "09122348",
-  },
-  {
-    id: "asc0asa23",
-    nombre: "Kevin",
-    numero: "09122348",
-  },
-  {
-    id: "11",
-    nombre: "tt",
-    numero: "09122348",
-  },
-];
+const init = () => {
+  const infoContactos = localStorage.getItem("contactos");
+
+  return infoContactos ? JSON.parse(infoContactos) : [];
+};
 
 const Contactos = () => {
-  const [state, dispatch] = useReducer(ContactosReducer, contactos);
+  const [state, dispatch] = useReducer(ContactosReducer, [], init);
+
+  useEffect(() => {
+    localStorage.setItem("contactos", JSON.stringify(state));
+  }, [state]);
+
+  const [formWiew, setFormWiew] = useState(false);
   return (
     <>
       <div className="container mt-3">
-        <FormularioAdd dispatch={dispatch} />
+        <div className="row">
+          <div className="col">
+            <button
+              type="button"
+              onClick={() => setFormWiew(!formWiew)}
+              className="btn btn-warning"
+            >
+              {!formWiew ? "Ver Formulario" : "Ocultar Formulario"}
+            </button>
+          </div>
+          <div className="col">
+            {formWiew && <FormularioAdd dispatch={dispatch} />}
+          </div>
+        </div>
+
         <TablaContactos contactos={state} dispatch={dispatch} />
       </div>
     </>
